1c8b87bd3b1f311865b93ba2f674bed1420fcd52
natthasath <natthasath.sak@gmail.com>
2025-03-17 13:59:05 +0700

    Rewrite code one time instance service

--- app/routers/tfa.py
+++ app/routers/tfa.py
@@ -9,26 +9,28 @@ router = APIRouter(
     responses={404: {"message": "Not found"}}
 )
 
+tfa_service = TfaService()
+
 @router.post("/totp/generate")
 async def totp_generate(data: GenerateSchema = Depends(GenerateSchema)):
-    return TfaService().totp_generate(data.email, data.qrcode)
+    return tfa_service.totp_generate(data.email, data.qrcode)
 
 @router.post("/totp/verify")
 async def totp_verify(data: VerifyTotpSchema = Depends(VerifyTotpSchema)):
-    return TfaService().totp_verify(data.secret, data.code)
+    return tfa_service.totp_verify(data.secret, data.code)
 
 @router.post("/hotp/generate")
 async def hotp_generate(data: GenerateSchema = Depends(GenerateSchema)):
-    return TfaService().hotp_generate(data.email, data.qrcode)
+    return tfa_service.hotp_generate(data.email, data.qrcode)
 
 @router.post("/hotp/regenerate")
 async def hotp_regenerate(data: ReGenerateSchema = Depends(ReGenerateSchema)):
-    return TfaService().hotp_regenerate(data.secret, data.count)
+    return tfa_service.hotp_regenerate(data.secret, data.count)
 
 @router.post("/hotp/verify")
 async def hotp_verify(data: VerifyHotpSchema = Depends(VerifyHotpSchema)):
-    return TfaService().hotp_verify(data.secret, data.code, data.count)
+    return tfa_service.hotp_verify(data.secret, data.code, data.count)
 
 @router.post("/qrcode/scan")
 async def qrcode_scan(file: UploadFile):
-    return TfaService().qrcode_scan(file.file)
+    return tfa_service.qrcode_scan(file.file)
